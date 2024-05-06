6ae1287151771f16de142ad572580ae602182364
Ulaş İşibol <ulasisbl@gmail.com>
2024-05-06 20:52:14 +0300

    navbar update

---
+++ portfolio-app/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent]
+})
+export class AppComponent {
+  title = 'portfolio-app';
+}

f3528a09e40f037ebbe20cb9d933d36e1fcb4a36
Ulaş İşibol <ulasisbl@gmail.com>
2024-05-12 22:47:44 +0300

    works component was complated

--- portfolio-app/src/app/app.routes.ts
+++ portfolio-app/src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './main/home/home.component';
+import { WorksComponent } from './main/works/works.component';
+import { AboutMeComponent } from './main/about-me/about-me.component';
+import { ContactComponent } from './main/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    
+    { path: 'works', component: WorksComponent },
+    { path: 'about-me', component: AboutMeComponent },
+    { path: 'contact', component: ContactComponent },
+
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
-  })
+})
 
-  export class AppRoutingModule { }
+export class AppRoutingModule { }

69c22f529b56051bc3df1625a94e2671358527a1
Ulaş İşibol <ulasisbl@gmail.com>
2024-05-15 21:56:52 +0300

    admin and adminSignUp components are generated

---
+++ portfolio-app/src/app/admin/admin-sign-up/admin-sign-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-sign-up.component.html',
+  styleUrl: './admin-sign-up.component.css'
+})
+export class AdminSignUpComponent {
+
+}
